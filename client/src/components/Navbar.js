82a0040c9d923026a3ce58b78dce229c8df0b56b
Christoph551 <christophersimmonds551@gmail.com>
2023-03-11 00:26:13 -0700

    Updated navbar with styling. Applied an mp4 video on a loop as a background, imported google fonts, added custom CSS to apply hover behavior on hav buttons, added gradient background color.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-
-
-const pages = ['Home', 'Profile', 'Gauntlet', 'About', 'Login/Signup' ];
+import FireNav from '../assets/animations/fireNav.mp4'
+const pages = ['Home', 'Profile', 'Gauntlet', 'About' ];
 
 export default function Navbar() {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -17,20 +16,41 @@ export default function Navbar() {
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
+                <div 
+                className="container-fluid"
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column'
+                }}
+                >
+                    <video autoPlay loop muted
+                    style={{
+                        position: 'absolute',
+                        width: "100%",
+                        left: "50%",
+                        top: "50%",
+                        height: "90%",
+                        objectFit: "cover",
+                        transform: "translate(-50%, -50%)",
+                        zIndex: "-1"
+                    }}
+                    >
+                        <source src={FireNav} type="video/mp4"/>
+                        </video>
+                    
                     <a className="navbar-brand" href="/">Gauntlet</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+
                     <div style={{
                         display: 'flex',
-                        justifyContent: 'flex-end'
+                        justifyContent: 'space-between'
                     }}
                     className="collapse navbar-collapse" 
                     id="navbarSupportedContent"
                     
                     >
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <ul 
+                        
+                        className="navbar-nav me-auto mb-2 mb-lg-0">
                             {pages.map((page) => {
                                 <li key={page} onClick={handleCloseNavMenu}>
                                     <a textAlign="center">
@@ -41,19 +61,20 @@ export default function Navbar() {
                                 </li>
                             })}
 
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/Arena">Arena</a>
+                            <li className="nav-item mx-5">
+                                <a className="nav-link" aria-current="page" href="/Arena">Arena</a>
                             </li>
-                            <li className="nav-item">
+                            <li className="nav-item mx-5">
                                 <a className="nav-link" href="/Profile">Profile</a>
                             </li>
-                            <li className="nav-item">
+                            <li className="nav-item mx-5">
                                 <a className="nav-link" href="/About">About</a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </nav>
+            
         </>
     )
 }
