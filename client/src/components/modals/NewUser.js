71eb24d633ff11ba7fd62e6f13181abff2612317
Christoph551 <christophersimmonds551@gmail.com>
2023-03-14 12:29:57 -0700

    Saving changes to arena and about dev pages.

--- client/src/components/modals/NewUser.js
+++ client/src/components/modals/NewUser.js
@@ -20,18 +20,18 @@ export default function NewUserModal() {
             </Button>
 
             <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
+                <Modal.Header style={{ backgroundColor: 'black' }} closeButton>
                     <Modal.Title
-                        style={{ color: 'black' }}
+                        style={{ color: '#3ae410e5' }}
                     >
                         Log In
                     </Modal.Title>
                 </Modal.Header>
-                <Modal.Body>
+                <Modal.Body style={{ backgroundColor: 'black' }}>
                     <Form>
                         <Form.Group className="mb-3" id="emailInput">
                             <Form.Label
-                            style={{ color: 'black' }}
+                            style={{ color: '#3ae410e5' }}
                             >
                                 Email address
                                 </Form.Label>
@@ -46,7 +46,7 @@ export default function NewUserModal() {
                             id="passwordInput"
                         >
                             <Form.Label
-                            style={{ color: 'black' }}
+                            style={{ color: '#3ae410e5' }}
                             >
                                 Password
                             </Form.Label>
@@ -54,11 +54,11 @@ export default function NewUserModal() {
                         </Form.Group>
                     </Form>
                 </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
+                <Modal.Footer style={{ backgroundColor: 'black' }}>
+                    <Button variant="secondary" onClick={handleClose} className="loginBtn btn-secondary">
                         Close
                     </Button>
-                    <Button variant="primary" onClick={handleClose} id="loginBtn">
+                    <Button variant="primary" onClick={handleClose} id="loginBtn" className="loginBtn btn-secondary">
                         Login
                     </Button>
                 </Modal.Footer>
--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,8 +1,46 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Chris from '../../assets/images/devs/chris.jpg';
 
-export default function About () {
+export default function About() {
     return (
         <>
-            About The Devs
+            <Container>
+                <h2>
+                    About The Developers
+                </h2>
+                <Row className="arena">
+                    <Col xs={6}>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={Chris} />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Github Profile</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col xs={6}>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Github Profile</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
