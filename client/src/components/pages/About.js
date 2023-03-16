d30c2ac5830380331bb1e45ff698a502bb0e978a
Anthony Walker <awalker2411@yahoo.com>
2023-03-16 03:05:28 -0400

    added info for myself in about, combat now functions but the character numbers are randomly coming back NaN

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -4,6 +4,7 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Chris from '../../assets/images/devs/chris.jpg';
+import Anthony from '../../assets/images/devs/anthonyWalker.png'
 
 export default function About() {
     return (
@@ -14,7 +15,7 @@ export default function About() {
                 </h2>
                 <Row>
                     <Col xs={6} className='aboutDevs'>
-                        <Card style={{ width: '18rem', backgroundColor: 'black' }}>
+                        <Card style={{ width: '30rem', backgroundColor: 'black' }}>
                             <Card.Img variant="top" src={Chris} />
                             <Card.Body>
                                 <Card.Title>Chris Simmonds</Card.Title>
@@ -32,13 +33,14 @@ export default function About() {
                         </Card>
                     </Col>
                     <Col xs={6} className='aboutDevs'>
-                        <Card style={{ width: '18rem', backgroundColor: 'black' }}>
-                            <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card style={{ width: '30rem', backgroundColor: 'black' }}>
+                            <Card.Img variant="top" src={Anthony} />
                             <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
+                                <Card.Title>Anthony Walker</Card.Title>
                                 <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
+                                    Full Stack Web Developer. Working with Mr. Simmonds was an absolute blast! I handled most of back-end, with a large focus on the combot logic for our Arena. It's been an extremely challenging task, but thankfully, I'm always up for a challenge!
+                                    <br></br>
+                                    If you're interested in my work, check out my Github below.
                                 </Card.Text>
                                 <Button className="Btn btn-secondary">
                                     <a target="_blank" href="https://github.com/awalker2411">
--- client/src/utils/combat.js
+++ client/src/utils/combat.js
@@ -1,5 +1,5 @@
-class Character{
-    constructor(health, attack, defense, speed){
+class Character {
+    constructor(health, attack, defense, speed) {
         this.health = health
         this.attack = attack
         this.attackLow = 1
@@ -10,146 +10,171 @@ class Character{
         this.speed = speed
     }
 
-    async attackVal(){
+    async attackVal() {
         const min = this.attackLow
         const max = this.attackHigh
-        let atkVal = Math.floor(Math.random()*(max - min + 1)) + min
+        let atkVal = Math.floor(Math.random() * (max - min + 1)) + min
         return atkVal
     }
 
-    async defenseVal(){
+    async defenseVal() {
         const min = this.defenseLow
         const max = this.defenseHigh
-        let defVal = Math.floor(Math.random()*(max - min + 1)) + min
+        let defVal = Math.floor(Math.random() * (max - min + 1)) + min
         return defVal
     }
 }
 
-class Gauntlet{
-    constructor(player1, enemy){
+class Gauntlet {
+    constructor(player1, enemy, turn, waves) {
+
         this.player1 = player1
         this.enemy = enemy
-        this.turn = 0
-        this.waves = 0
 
         this.enemyScale_healthScale = 1.1
         this.enemyScale_atkScale = 1.1
         this.enemyScale_defScale = 1.1
         this.enemyScale_spdScale = 1.1
     }
+}
 
-    async combatTurn(){
-        this.turn++
-        if(this.player1.speed > this.enemy.speed || this.player1.speed === this.enemy.speed){
-            let atkRoll = this.player1.attackVal()
-            let defRoll = this.enemy.attackVal()
-            if(this.player1.speed > 2*this.enemy.speed || this.player1.speed === 2*this.enemy.speed){
-                const secondAtkRoll = this.player1.attackVal()
-                atkRoll+= secondAtkRoll
+function combatGame(gauntlet) {
+    let waves = 0
+    let turn = 0
+
+    function combatTurn(turn, player1, enemy) {
+        turn++
+        if (player1.speed > enemy.speed || player1.speed === enemy.speed) {
+            let atkRoll = player1.attackVal()
+            let defRoll = enemy.attackVal()
+            if (player1.speed > 2 * enemy.speed || player1.speed === 2 * enemy.speed) {
+                const secondAtkRoll = player1.attackVal()
+                atkRoll += secondAtkRoll
             }
             let healthHit = atkRoll - defRoll
-            if(healthHit < 0){ //line 61
+            if (healthHit < 0) { //line 61
                 healthHit = 0
             }
             // Need to add self damage for not breaking through defense; line 63
-            this.enemy.health-= healthHit
-            if(this.enemy.health > 0){
-                atkRoll = this.enemy.attackVal()
-                defRoll = this.enemy.defenseVal()
+            enemy.health -= healthHit
+            if (enemy.health > 0) {
+                atkRoll = enemy.attackVal()
+                defRoll = enemy.defenseVal()
                 healthHit = atkRoll - defRoll
-                if(healthHit < 0){
+                if (healthHit < 0) {
                     healthHit = 0
                 }
                 // Need to add self damage for not breaking through defense; line 79
-                this.player1.health-= healthHit
+                this.player1.health -= healthHit
             }
-        }else{
-            let atkRoll = this.enemy.attackVal()
-            let defRoll = this.player1.defenseVal()
-            if(this.enemy.speed > 2*this.player1.speed || this.enemy.speed === 2*this.player1.speed){
-                const secondAtkRoll = this.enemy.attackVal()
-                atkRoll+= secondAtkRoll
+        } else {
+            let atkRoll = enemy.attackVal()
+            let defRoll = player1.defenseVal()
+            if (enemy.speed > 2 * player1.speed || enemy.speed === 2 * player1.speed) {
+                const secondAtkRoll = enemy.attackVal()
+                atkRoll += secondAtkRoll
             }
             let healthHit = atkRoll - defRoll
-            if(healthHit < 0){ //line 96
+            if (healthHit < 0) { //line 96
                 healthHit = 0
             }
             // Need to add self damage for not breaking through defense; line 63
-            this.player1.health-= healthHit
-            if(this.player1.health > 0){
-                atkRoll = this.player1.attackVal()
-                defRoll = this.player1.defenseVal()
+            player1.health -= healthHit
+            if (player1.health > 0) {
+                atkRoll = player1.attackVal()
+                defRoll = player1.defenseVal()
                 healthHit = atkRoll - defRoll
-                if(healthHit < 0){
+                if (healthHit < 0) {
                     healthHit = 0
                 }
                 // Need to add self damage for not breaking through defense; line 79
-                this.enemy.health-= healthHit
+                enemy.health -= healthHit
             }
         }
     }
-    
-    async combatWave(){
-        this.waves++
-        if(this.player1.health > 0 && this.enemy.health > 0){
-            function fight(){
-                this.combatTurn()
-                if(this.player1.health > 0 && this.enemy.health > 0){
+
+    function combatWave(waves, player1, enemy, turn) {
+        waves++
+        if (player1.health > 0 && enemy.health > 0) {
+            function fight() {
+                combatTurn(turn, player1, enemy)
+                if (player1.health > 0 && enemy.health > 0) {
                     fight()
                 }
             }
             fight()
         }
         let scaleEnemy = Math.floor(Math.random() * 4)
-        if(scaleEnemy === 0){
-            this.enemy.attack = this.enemy.attack*this.enemy.enemyScale_atkScale
-        }else if(scaleEnemy === 1){
-            this.enemy.health = this.enemy.health*this.enemy.enemyScale_healthScale
-        }else if(scaleEnemy === 2){
-            this.enemy.defense = this.enemy.defense*this.enemy.enemyScale_defScale
-        }else{
-            this.enemy.speed = this.enemy.speed*this.enemy.enemyScale_spdScale
+        if (scaleEnemy === 0) {
+            enemy.attack = enemy.attack * enemy.enemyScale_atkScale
+        } else if (scaleEnemy === 1) {
+            enemy.health = enemy.health * enemy.enemyScale_healthScale
+        } else if (scaleEnemy === 2) {
+            enemy.defense = enemy.defense * enemy.enemyScale_defScale
+        } else {
+            enemy.speed = enemy.speed * enemy.enemyScale_spdScale
         }
     }
 
-    async combatGame(){
-        if(this.player1.health > 0){
-            function game(){
-                this.combatWave()
-                if(this.player1.health > 0){
-                    game()
-                }
+    if (gauntlet.player1.health > 0) {
+        function game(gauntlet, index) {
+            if (index >= 50) {
+                return;
             }
-            game()
-            console.log(`Your character made it to wave `+this.waves+`!`)
-            localStorage.setItem('waves', this.waves)
+            index++
+            combatWave(waves, gauntlet.player1, gauntlet.enemy, turn)
+            if (gauntlet.player1.health > 0) {
+                index++
+                game(gauntlet, index)
+            }
         }
+        game(gauntlet, 1)
+        console.log(`Your character made it to wave ` + waves + `!`)
+        localStorage.setItem('waves', waves)
     }
 }
 
 
-export default function createCharacter(characterType){
-    let waves = localStorage.getItem('waves')||0
+export default function createCharacter(characterType) {
+    let waves = localStorage.getItem('waves') || 0
     let player1;
+    let enemy
 
-    switch(characterType){
+    switch (characterType) {
         case "Arcanist":
             player1 = new Character(150, 40, 30, 10)
+            enemy = new Character(200, 20, 20, 20)
+            break;
         case "Brute":
             player1 = new Character(600, 100, 20, 10)
+            enemy = new Character(200, 20, 20, 20)
+            break;
         case "Bulwark":
             player1 = new Character(1000, 17.5, 50, 5)
+            enemy = new Character(200, 20, 20, 20)
+            break;
         case "Dread Knight":
             player1 = new Character(200, 30, 30.5, 10)
+            enemy = new Character(200, 20, 20, 20)
+            break;
         case "Engineer":
             player1 = new Character(100, 69, 17, 25)
+            enemy = new Character(200, 20, 20, 20)
+            break;
         case "Shadowblade":
             player1 = new Character(50, 45, 10, 75)
+            enemy = new Character(200, 20, 20, 20)
+            break;
+        default:
+            player1 = new Character(200, 30, 30.5, 10)
+            enemy = new Character(200, 20, 20, 20)
     }
 
-    const enemy = new Character(200, 20, 20, 20)
+    console.log(enemy)
+    console.log(player1)
 
-    const Arena = new Gauntlet(player1, enemy).combatGame()
+    const Arena = new Gauntlet(player1, enemy, 0, 0)
+    combatGame(Arena)
     console.log(Arena)
     //Arena.combatGame()
 }
